090878523d271b9575f592ade3b863d1ad56542a
Boston Vanseghi <bvanseg@gmail.com>
2022-12-10 21:30:04 -0500

    Added remaining x0 functions.

--- src/opcode/x0.rs
+++ src/opcode/x0.rs
@@ -46,4 +46,71 @@ impl LR35902 {
         self.bc.upper = next;
         self.cycles += 8;
     }
+
+    pub fn x07_rlc_a(&mut self) {
+        // TODO: Rotate left circular A register
+        self.cycles += 4;
+    }
+
+    pub fn x08_ld_a16_sp(&mut self) {
+        // TODO:
+        self.cycles += 20;
+    }
+
+    pub fn x09_add_hl_bc(&mut self) {
+        let snapshot = self.hl.get();
+        self.hl.add(self.bc.get());
+
+        // Handle flags.
+        self.set_n_flag(false);
+        self.check_h_flag(true, snapshot, self.bc.get());
+        // TODO: Check carry flag.
+
+        self.cycles += 8;
+    }
+
+    pub fn x0a_ld_a_bc(&mut self) {
+        // TODO:
+        // BC is a pointer, dereference and load byte value from memory into A register.
+        self.cycles += 8;
+    }
+
+    pub fn x0b_dec_bc(&mut self) {
+        self.bc.sub(1);
+        self.cycles += 8;
+    }
+
+    pub fn x0c_inc_c(&mut self) {
+        let snapshot = self.bc.lower;
+        self.bc.lower += 1;
+
+        self.check_z_flag(self.bc.lower as u16);
+        self.set_n_flag(false);
+        self.check_h_flag(true, snapshot as u16, 1);
+
+        self.cycles += 4;
+    }
+
+    pub fn x0d_dec_c(&mut self) {
+        let snapshot = self.bc.lower;
+        self.bc.lower -= 1;
+
+        self.check_z_flag(self.bc.lower as u16);
+        self.set_n_flag(true);
+        self.check_h_flag(false, snapshot as u16, 1);
+
+        self.cycles += 4;
+    }
+
+    pub fn x0e_ld_c_imm8(&mut self) {
+        let next = self.fetch();
+        self.bc.lower = next;
+
+        self.cycles += 4;
+    }
+
+    pub fn x0f_rrc_a(&mut self) {
+        // TODO: Rotate right circular A register
+        self.cycles += 4;
+    }
 }
